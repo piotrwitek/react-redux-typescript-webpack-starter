775f78dcde94091ab430db1ad83092a2aaef5696
Piotr Witek <piotrek.witek@gmail.com>
2017-04-30 11:45:39 +0200

    react-hot-loader dependency removed from prod bundle

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,42 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 
 import { App } from './app';
 
-const render = (Component: any) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('root'),
-  );
+const renderRoot = (app: JSX.Element) => {
+  ReactDOM.render(app, document.getElementById('root'));
 };
 
-render(App);
+if (process.env.NODE_ENV === 'production') {
+  renderRoot((
+    <App />
+  ));
+} else { // removed in production, hot-reload config
+  const HotContainer = require('react-hot-loader').AppContainer;
+  renderRoot((
+    <HotContainer>
+      <App />
+    </HotContainer>
+  ));
 
-// development hot-reload configs
-if (module.hot) {
-  // app
-  module.hot.accept('./app', async () => {
-    // const NextApp = require('./app').App;
-    const NextApp = (await System.import('./app')).App;
-    render(NextApp);
-  });
+  if (module.hot) {
+    // app
+    module.hot.accept('./app', async () => {
+      // const NextApp = require('./app').App;
+      const NextApp = (await System.import('./app')).App;
+      renderRoot((
+        <HotContainer>
+          <NextApp />
+        </HotContainer>
+      ));
+    });
 
-  // store
-  // module.hot.accept('./redux/reducers', () => {
-  //   const reducers = require('./redux/reducers').default;
-  //   return store.replaceReducer(reducers);
-  // });
+    // store
+    // module.hot.accept('./redux/reducers', () => {
+    //   const reducers = require('./redux/reducers').default;
+    //   return store.replaceReducer(reducers);
+    // });
 
-  // epics
+    // epics
+  }
 }
